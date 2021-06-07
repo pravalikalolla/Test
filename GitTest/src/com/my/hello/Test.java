91e95db7e21e172bdeaa5865dd4dc25312739656
pravalikalolla <pravalikalolla@gmail.com>
2021-06-07 12:32:06 +0530

    commit using Eclipse

--- GitTest/src/com/my/hello/Test.java
+++ GitTest/src/com/my/hello/Test.java
@@ -3,6 +3,7 @@ package com.my.hello;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Test");
+		System.out.println("Hello");
 		System.out.println("You are in Master");
 		System.out.println("In front end branch");	}
 }
