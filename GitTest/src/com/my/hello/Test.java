ce8b3f51f7ed2413c4f23661b94da16a0a57c558
pravalikalolla <pravalikalolla@gmail.com>
2021-06-07 12:33:40 +0530

    Changes made in Eclipse

--- GitTest/src/com/my/hello/Test.java
+++ GitTest/src/com/my/hello/Test.java
@@ -2,7 +2,6 @@ package com.my.hello;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Test");
 		System.out.println("Hello");
 		System.out.println("You are in Master");
 		System.out.println("In front end branch");	}
